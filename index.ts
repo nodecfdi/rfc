883dbd244c22fb6b89eb6aee13692006f055c829
Fernando Isidro Luna <luffynando@gmail.com>
2024-04-27 15:59:22 -0600

    perf: :zap: move exports to isolated index file in root

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { default as CheckSum } from './src/check_sum.js';
+export { default as Rfc } from './src/rfc.js';
+export { default as RfcFaker } from './src/rfc_faker.js';
+export { default as RfcIntConverter } from './src/rfc_int_converter.js';
+export { default as RfcParser } from './src/rfc_parser.js';
