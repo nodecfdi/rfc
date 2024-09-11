b94afb88ee4bc1b4a98a6a9d9e9aef675b9cd1ea
Fernando IL <luffynando@gmail.com>
2024-09-11 13:50:12 -0600

    refactor: :wastebasket: remove old config and unused file configs dirs and files

--- index.ts
+++
@@ -1,5 +0,0 @@
-export { CheckSum } from './src/check_sum.js';
-export { Rfc } from './src/rfc.js';
-export { RfcFaker } from './src/rfc_faker.js';
-export { RfcIntConverter } from './src/rfc_int_converter.js';
-export { RfcParser } from './src/rfc_parser.js';
